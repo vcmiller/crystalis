cebe375a432d655a5db48f226d9dfc9bb28e4dd8
Vincent Miller <vmiller0725@gmail.com>
2016-10-01 01:45:48 -0400

    asdf

---
+++ Crystalis/Assets/Models/Boat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up * 4, Vector3.down, out hit)) {//Physics.BoxCast(transform.position + Vector3.up * 4, new Vector3(.6f, .2f, 1.5f), Vector3.down, out hit, Quaternion.identity)) {
+            
+            
+            Vector3 pos = transform.position;
+            pos.y = hit.point.y;
+            transform.position = pos;
+            transform.up = Vector3.RotateTowards(transform.up, hit.normal, Time.deltaTime * .15f, 0);
+        }
+
+
+	}
+}
--- Crystalis/Assets/Models/Water.cs
+++ Crystalis/Assets/Models/Water.cs
@@ -13,22 +13,24 @@ public class Water : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         noise = new OpenSimplexNoise();
-        mesh = GetComponent<MeshFilter>().mesh;
-	}
+        GetComponent<MeshFilter>().sharedMesh = Instantiate(GetComponent<MeshFilter>().sharedMesh);
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3[] verts = mesh.vertices;
         for (int i = 0; i < mesh.vertexCount; i++) {
             Vector3 v = verts[i];
-            Vector3 pos = v;
-            pos.x += transform.position.x;
-            pos.y -= transform.position.z;
-            v.z = (float)noise.Evaluate(pos.x * posScale, pos.y * posScale, Time.time * timeScale) * noiseScale;
-            v.z += (float)noise.Evaluate(pos.x * posScale * .25f, pos.y * posScale * .25f, Time.time * timeScale * .5f) * noiseScale * 2;
+            Vector3 pos = v + transform.position;
+            v.y = (float)noise.Evaluate(pos.x * posScale, Time.time * timeScale, pos.z * posScale) * noiseScale;
+            //v.y += (float)noise.Evaluate(pos.x * posScale * .25f, pos.z * posScale * .25f, Time.time * timeScale * .5f) * noiseScale * 2;
             verts[i] = v;
         }
         mesh.vertices = verts;
         mesh.RecalculateNormals();
+        GetComponent<MeshCollider>().sharedMesh = mesh;
 	}
 }
