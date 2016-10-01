901f63730d2daec0255c71c21f7f4f0c3d145326
Vincent Miller <vmiller0725@gmail.com>
2016-10-01 16:16:53 -0400

    SHIT FUCK

--- Crystalis/Assets/Scripts/RigidbodyAI.cs
+++ Crystalis/Assets/Scripts/RigidbodyAI.cs
@@ -3,21 +3,17 @@ using System.Collections;
 
 public class RigidbodyAI : MonoBehaviour {
     Rigidbody rb;
-    Waypoint waypoint;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        Vector3 v = transform.forward * 3;
+        v.y = 1.0f / transform.position.y;
+        rb.velocity = v;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (waypoint != null) {
-            if (Vector3.SqrMagnitude(transform.position - waypoint.transform.position) < 4) {
-                waypoint = waypoint.next;
-                return;
-            }
-        }
 
         Vector3 v = transform.eulerAngles;
         v.x = rb.velocity.y * -2;
